50fa8141209f0fa0c95a7cb8738a238ed9183851
Alberto Calmini <albycalmini99@gmail.com>
2024-09-15 14:27:35 +0200

    aggiornata interfaccia in accordo al DB - avanzamento creazione pagine manifest

--- static/manifest.js
+++ static/manifest.js
@@ -19,9 +19,23 @@ function manifestOggi() {
         async init() {
             let response = await fetch("/manifest_oggi");
             this.manifest_oggi = await response.json();
+        },
+        async refreshManifest() {
+            let response = await fetch("/manifest_oggi");
+            this.manifest_oggi = await response.json();
         }
     }
 }
+
+function refresh_automatico() {
+    const manifestElement = document.getElementById('manifest');
+    const manifestComponent = Alpine.$data(manifestElement);
+
+    if (manifestComponent) {
+        manifestComponent.refreshManifest();
+    }
+}
+setInterval(refresh_automatico, 20000);
 
 function manifestStorico() {
     return {
@@ -135,6 +149,371 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (modalComponent) {
                 modalComponent.loadData(data, todo);
+            }
+        });
+    }
+});
+
+function setDecollo(id_decollo) {
+    const url = new URL(window.location);
+    url.searchParams.set('decollo_id', id_decollo);
+    window.location.href = url;
+
+}
+
+function getDecollo() {
+    return {
+        manifest_oggi: false,
+        decollo_selezionato: false,
+        async init() {
+            const params = new URLSearchParams(window.location.search);
+            const decolloId = params.get('decollo_id');
+
+            let response = await fetch("/manifest_oggi");
+            this.manifest_oggi = await response.json();
+            for (let item of this.manifest_oggi.response.decolli) {
+                if (item.id == decolloId) {
+                    this.decollo_selezionato = item;
+                }
+            }
+        },
+        async refreshDecollo() {
+            const params = new URLSearchParams(window.location.search);
+            const decolloId = params.get('decollo_id');
+
+            let response = await fetch("/manifest_oggi");
+            this.manifest_oggi = await response.json();
+            for (let item of this.manifest_oggi.response.decolli) {
+                if (item.id == decolloId) {
+                    this.decollo_selezionato = item;
+                }
+            }
+        }
+    }
+}
+
+function unsetDecollo(id_decollo) {
+    const url = new URL(window.location);
+    url.search = '';
+    window.location.href = url;
+}
+
+function editDecollo() {
+    return {
+        async aggiungiDecollo(giornata_id) {
+            console.log(giornata_id);
+            fetch("/edit_decollo", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                  Accept: "application/json",
+                },
+                body: JSON.stringify({
+                    "id_giornata": giornata_id,
+                    "todo": "crea",
+                }),
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                if (data.response != "error") {
+
+                    document.getElementById("refresh_manifest").click();
+                    setTimeout(() => {
+                        const dynamicButtonId = `edit_decollo-${data.response}`;
+                        const button = document.getElementById(dynamicButtonId);
+                        if (button) {
+                            button.click();
+                        } else {
+                            console.error("Pulsante con ID dinamico non trovato:", dynamicButtonId);
+                        }
+                    }, 500);
+                }
+            })
+        }
+    }
+}
+
+function aggiungiTDForm() {
+    return {
+        formData: {
+            todo: "",
+            decollo_id: "",
+            td_id: "",
+            pilota_td: 1,
+            passeggero: "",
+            video_polso: false,
+            video_ext: false,
+            foto: false,
+            videoman: 1,
+        },
+        elenco_piloti_td: [],
+        elenco_videoman: [],
+        formMessage: "",
+            formLoading: false,
+                init() {
+                    this.$watch('formData.ip', value => {
+                        if (value) {
+                            this.formData.dl = true;
+                        }
+                    });
+                },
+                loadData(decolloid, tdid) {
+                    if (!tdid) {
+                        this.formData.todo = "crea_td";
+                        this.formData.decollo_id = decolloid;
+                        this.formData.td_id = "";
+                        fetch(`/paracadutisti`)
+                        .then(response => response.json())
+                        .then(data => {
+                            for (let i in data.response) {
+                                if (data.response[i].licenza && data.response[i].licenza.ip && data.response[i].licenza.ip.td) {
+                                    this.elenco_piloti_td.push(data.response[i]);
+                                }
+                            }
+                            for (let i in data.response) {
+                                if (data.response[i].videoman) {
+                                    this.elenco_videoman.push(data.response[i]);
+                                }
+                            }
+                        });
+                    } else {
+                        // Codice da eseguire se tdid è definito (non specificato nell'esempio)
+                    }
+                },
+                submitForm() {
+                    this.formMessage = "";
+                    this.formLoading = true;
+                    fetch("/edit_decollo", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                          Accept: "application/json",
+                        },
+                        body: JSON.stringify(this.formData),
+                    })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then((data) => {
+                        if (data.response == "ok") {
+                            document.getElementById("refresh_decollo").click();
+                        }
+                    })
+                    .finally(() => {
+                        this.formLoading = false;
+                    });
+                },
+    };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const aggiungiTDModal = document.getElementById('aggiungiTDModal');
+
+    if (aggiungiTDModal) {
+        aggiungiTDModal.addEventListener('show.bs.modal', event => {
+            const button = event.relatedTarget;
+            const decolloid = button.getAttribute('data-decolloid');
+            const tdid = button.getAttribute('data-tdid');
+
+            const modalComponent = Alpine.$data(aggiungiTDModal.querySelector('[x-data]'));
+
+            if (modalComponent) {
+                modalComponent.loadData(decolloid, tdid);
+            }
+        });
+    }
+});
+
+
+function aggiungiAFFForm() {
+    return {
+        formData: {
+            todo: "",
+            decollo_id: "",
+            aff_id: "",
+            livello_aff: 1,
+            ip_primario: 1,
+            allievo: 2,
+            ip_secondario: 1,
+        },
+        elenco_ip_aff: [],
+        elenco_allievi: [],
+        livelli_aff: [1,2,3,4,5,6,7],
+        formMessage: "",
+            formLoading: false,
+                loadData(decolloid, affid) {
+                    if (!affid) {
+                        this.formData.todo = "crea_aff";
+                        this.formData.decollo_id = decolloid;
+                        this.formData.aff_id = "";
+                        fetch(`/paracadutisti`)
+                        .then(response => response.json())
+                        .then(data => {
+                            for (let i in data.response) {
+                                if (data.response[i].licenza && data.response[i].licenza.ip && data.response[i].licenza.ip.aff) {
+                                    this.elenco_ip_aff.push(data.response[i]);
+                                }
+                            }
+                            for (let i in data.response) {
+                                if (data.response[i].licenza && data.response[i].licenza.numero == "") {
+                                    this.elenco_allievi.push(data.response[i]);
+                                }
+                            }
+                            if (this.elenco_allievi.length === 1) {
+                                this.formData.allievo = this.elenco_allievi[0].id;
+                            }
+                        });
+                    } else {
+                        // Codice da eseguire se affid è definito (non specificato nell'esempio)
+                    }
+                },
+                submitForm() {
+                    this.formMessage = "";
+                    this.formLoading = true;
+                    fetch("/edit_decollo", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                          Accept: "application/json",
+                        },
+                        body: JSON.stringify(this.formData),
+                    })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then((data) => {
+                        if (data.response == "ok") {
+                            document.getElementById("refresh_decollo").click();
+                        }
+                    })
+                    .finally(() => {
+                        this.formLoading = false;
+                    });
+                },
+    };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const aggiungiAFFModal = document.getElementById('aggiungiAFFModal');
+
+    if (aggiungiAFFModal) {
+        aggiungiAFFModal.addEventListener('show.bs.modal', event => {
+            const button = event.relatedTarget;
+            const decolloid = button.getAttribute('data-decolloid');
+            const affid = button.getAttribute('data-affid');
+
+            const modalComponent = Alpine.$data(aggiungiAFFModal.querySelector('[x-data]'));
+
+            if (modalComponent) {
+                modalComponent.loadData(decolloid, affid);
+            }
+        });
+    }
+});
+
+
+function aggiungiParaForm() {
+    return {
+        formData: {
+            todo: "",
+            decollo_id: "",
+            para_id: "",
+            paracadutista: 1,
+            dl: false,
+            disciplina: 1,
+        },
+        elenco_para: [],
+        elenco_discipline: [],
+        formMessage: "",
+        formLoading: false,
+            init() {
+                this.$watch('formData.dl', value => {
+                    if (value) {
+                        this.formData.dl = true;
+                    }
+                });
+            },
+            loadData(decolloid, paraid) {
+                this.elenco_para = [];
+                this.elenco_discipline = [];
+                if (!paraid) {
+                    this.formData.todo = "crea_para";
+                    this.formData.decollo_id = decolloid;
+                    this.formData.para_id = "";
+                    this.formData.paracadutista = 1;
+                    this.formData.dl = false;
+                    this.formData.disciplina = 1;
+                    fetch(`/paracadutisti`)
+                    .then(response => response.json())
+                    .then(data => {
+                        for (let i in data.response) {
+                            this.elenco_para.push(data.response[i]);
+                        }
+                    });
+                    fetch(`/discipline`)
+                    .then(response => response.json())
+                    .then(data => {
+                        for (let i in data.response) {
+                            this.elenco_discipline.push(data.response[i]);
+                        }
+                    });
+                } else {
+                    // Codice da eseguire se paraid è definito (non specificato nell'esempio)
+                }
+            },
+            submitForm() {
+                this.formMessage = "";
+                this.formLoading = true;
+                fetch("/edit_decollo", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                    body: JSON.stringify(this.formData),
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    if (data.response == "ok") {
+                        document.getElementById("refresh_manifest").click();
+                    }
+                })
+                .finally(() => {
+                    this.formLoading = false;
+                });
+            },
+    };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const aggiungiParaModal = document.getElementById('aggiungiParaModal');
+
+    if (aggiungiParaModal) {
+        aggiungiParaModal.addEventListener('show.bs.modal', event => {
+            const button = event.relatedTarget;
+            const decolloid = button.getAttribute('data-decolloid');
+            const paraid = button.getAttribute('data-paraid');
+
+            const modalComponent = Alpine.$data(aggiungiParaModal.querySelector('[x-data]'));
+
+            if (modalComponent) {
+                modalComponent.loadData(decolloid, paraid);
             }
         });
     }
--- static/sidebar.js
+++ static/sidebar.js
@@ -1,9 +1,13 @@
 function sidebarData() {
     return {
         data: '',
+        id_enac: localStorage.getItem("id_enac"),
+        username: localStorage.getItem("username"),
         async init() {
             let response = await fetch("/sidebardata");
             this.data = await response.json();
+            localStorage.setItem("id_enac", this.data.response.id_enac);
+            localStorage.setItem("username", this.data.response.username);
         }
     }
 }
